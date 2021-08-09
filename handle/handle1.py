1a0765062e2215b50f7c2e8dfcbbb91db85db2c6
watcharis <m.watcharis@gmail.com>
2021-08-09 12:39:52 +0700

    update handle1 in master

--- handle/handle1.py
+++ handle/handle1.py
@@ -3,3 +3,8 @@ from flask import Flask, jsonify, request
 def hello():
     s = "hello world"
     return jsonify({"message": s}), 200
+
+
+def voice():
+    s = "blavlallva"
+    return jsonify({"message": s}), 200
